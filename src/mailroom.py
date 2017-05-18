a9e6bcf94be9d2929a1d21db3cbbdda181f4b702
Morgan Nomura <morganelle@gmail.com>
2017-05-18 14:50:08 -0700

    created choice function

--- src/mailroom.py
+++ src/mailroom.py
@@ -1,9 +1,34 @@
 """Mailroom Madness module lets a user track and thank donors and donations."""
 
 
+DONORS = {
+    'Anna Shelby': [300],
+    'Morgan Nomura': [10],
+    'Edgar Allan Poe': [1000]
+}
+
+
 def main():
     """Main function."""
+    user_prompt()
     print('MAILROOM MADNESS!!')
+
+
+def user_prompt():
+    """Function that prompts user to choose to write a thank you or build a report."""
+    print('Type Q to quit the program anytime')
+    print('Would you like to write a thank you email to a donor or see a report of past donations?')
+    choice = input('Type T for thank you or R for report: ').upper()
+    options = ['T', 'R', 'Q']
+    if choice not in options:
+        print('Sorry, there is no option for what you typed.')
+        choice = input('Please type T for thank you or R for report, or Q to quit:')
+    elif choice == 'R':
+        print('You chose report!!')
+    elif choice == 'T':
+        print('You chose THANK YOU EMAIL!!')
+    else:
+        pass
 
 
 if __name__ == '__main__':  # pragma no cover
