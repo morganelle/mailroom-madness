f224d0e999a1686c681df50a4e0c2fcd15765c4f
Morgan Nomura <morganelle@gmail.com>
2017-05-18 13:47:50 -0700

    added setup tools and added setup.py

---
+++ setup.py
@@ -0,0 +1,27 @@
+"""Setup file for Mailroom Madness package."""
+from setuptools import setup
+
+
+dependences = ['ipython']
+extra_packages = {
+    'testing': ['pytest', 'py-watch', 'pytest-cov', 'tox']
+}
+
+
+setup(
+    name='Mailroom Madness',
+    description='401 Python project',
+    version='0.1',
+    authors='Anna, Morgan',
+    author_email='morganelle@gmail.com',
+    license='MIT',
+    py_modules=['mailroom_madness', 'test_mailroom_madness'],
+    package_dir={'': 'src'},
+    install_requires=dependences,
+    extras_require=extra_packages,
+    entry_points={
+        'console_scripts': [
+            'mailroom_madness mailroom_madness:main'
+        ]
+    }
+)
