7a531f743f7dedaedd587a2ce0131cd396f0ca60
Morgan Nomura <morganelle@gmail.com>
2017-05-18 14:21:59 -0700

    added entry point, renamed module

---
+++ src/mailroom.py
@@ -0,0 +1,10 @@
+"""Mailroom Madness module lets a user track and thank donors and donations."""
+
+
+def main():
+    """Main function."""
+    print('MAILROOM MADNESS!!')
+
+
+if __name__ == '__main__':  # pragma no cover
+    main()
