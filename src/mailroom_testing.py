3daffc2ac405df4ab51b6e47800334601679fc1a
bonanashelby <bonanashelby@gmail.com>
2017-05-18 17:54:24 -0700

    we made a test for donor list

---
+++ src/mailroom_testing.py
@@ -0,0 +1,18 @@
+"""This is our tests for our mailroom madness program."""
+import pytest
+
+DONORS = {
+    'ANNA SHELBY': [300],
+    'MORGAN NOMURA': [10],
+    'EDGAR POE': [1000]
+}
+
+
+def test_validate_user_name_input():
+    """Test for checking the user input is a first and last name."""
+
+
+def test_show_list():
+    """Test for showing donors list is a list."""
+    from mailroom import show_list
+    assert show_list(DONORS) == ['ANNA SHELBY', 'MORGAN NOMURA', 'EDGAR POE']
